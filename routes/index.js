d30b32d9b42a1106300b828e4adda422e32cc3ba
Surajit Barman <surajitb@thoughtworks.com>
2015-01-08 21:33:50 +0530

    data can be submitted in database

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,31 @@ router.get('/', function(req, res){
 });
 
 
+router.get('/waitingPage', function(req , res){
+    res.render("waitingPage")
+})
+router.get('/create_quiz' , function(req,res){
+    res.render("create_quiz");
+})
+
+router.post('/create_quiz' , function(req,res){
+    var quiz_info = req.body;
+    if(req.body.email_id==undefined){
+    	quiz_info.email_id = "a@gmail.com";
+    }
+    if(req.body.status==undefined){
+    	quiz_info.status = "open";
+    }
+    if(req.body.total_questions==undefined){
+    	quiz_info.total_questions = "10";
+    }
+    console.log(quiz_info);
+    quiz_lib.add_new_quiz(quiz_info,function(error){
+    	error && console.log(error);
+    	!error && res.redirect("waitingPage");
+    });
+})
+
+
 
 module.exports = router;
--- tests/quiz_test.js
+++ tests/quiz_test.js
@@ -12,26 +12,69 @@ describe('quiz_records',function(){
 		quiz_lib = lib.init(TEST_DB_PATH);
 
 	});
-	describe("#show_open_quizzes",function(){
-		it("gives the list of all open quizzes",function(done){
-			var expected = [{id:1,name:"Science"},{id:3,name:"Biology"}];
-			//assert.equal([])
-		});
-	});
+	describe("#create_quiz",function(){
+		it("add a new quiz G.K.",function(done){
+			var quiz = {
+				name : 'G.K.',
+				email_id:"any@g.com",
+				hours:"0",
+				Minutes:"10",
+				Seconds:"0",
+				total_seats:25,
+				total_questions:1,
+				filename:'filename.txt',
+				status:'open'
+			};
+			quiz_lib.add_new_quiz(quiz,function(err){
+				assert.notOk(err);
+				done();
+			})
+		})
+		it("check insert quiz is in database or not",function(done){
+			var quiz1 = {
+				id:1,
+				name : 'Science',
+				email_id:"d@email.com",
+				total_time:"00:30:00",
+				total_seats:10,
+				total_questions:"6",
+				filename:'1.json',
+				status:'open'
+			};
+			var callback = function(error,QuizInfo){
+				assert.notOk(error);
+				assert.deepEqual(quiz1,QuizInfo);
+				done();
+			};
+			quiz_lib.get_quiz_info(callback);
+		})
+	})
 
 });
 
 
-// /////////
-// describe('#getMyTopics',function(){
-// 		it('getMyTopics get all created and joined topics of particular user',function(done){
-// 			var expected = [ { id: 2, name: 'step of success' },
-//   							{ id: 1, name: 'step' },
-//   							{ id: 3, name: 'soda' } ];
-// 			adda_records.getMyTopics(2,function(err,topics){
-// 				assert.notOk(err);
-// 				assert.deepEqual(topics,expected);
-// 				done();
-// 			});
+
+
+// describe('#addTopic',function(){
+// 	it('insert new topic into topics table along with userId and time',function(done){
+// 		var topic = {
+// 			name: 'hocky',
+// 			description: 'hocky is our national game',
+// 			userId: 1,
+// 			start_time:"GMT 15:30",
+// 		};
+
+// 		var callback = function(error,topicInfo){
+// 			assert.notOk(error);
+// 			topic.id = 4;
+// 			topic.end_time = null;
+// 			assert.deepEqual(topicInfo[3],topic);
+// 			done();
+// 		};
+
+// 		adda_records.addTopic(topic, function(err){
+// 			assert.notOk(err);
+// 			adda_records.getTopicInfo(callback);
 // 		});
 // 	});
+// });
