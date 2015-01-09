4fc33308cc986ef736bcc4139da94ae935cc5ceb
Dolly <dollyg@INDOLLYG.corporate.thoughtworks.com>
2015-01-09 14:29:19 +0530

    Full Story: #S101,homepage and login page Completed.

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({name:'Quiz',secret:"Its a secret."}));
+app.use(session({name:'Quiz',secret:"Its a secret"}));
 
 app.use('/', routes);
 
