d7fc63d178acb754b721fc8347fe53d925a3c59d
Jai Prakash <jaiprak@thoughtworks.com>
2015-01-10 14:53:51 +0530

    new changes have done by dolly

--- scripts/fill_sample_data.sql
+++ scripts/fill_sample_data.sql
@@ -7,5 +7,3 @@ insert into quizzes(name, email_id, total_time, total_seats, total_questions, fi
 		("Biology","g@email.com","00:45:00",20,10,"3.json","open"),
 		("Astrology","s@email.com","00:40:00",6,12,"4.json","close"),
 		("Chemistry","t@email.com","00:30:00",10,10,"5.json","running");
-insert into results(email_id, quiz_id, time_taken, rank, commits, corrects)
-	values("d@email.com",1,"00:21:00",1,6,5 );
