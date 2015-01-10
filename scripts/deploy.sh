9963cdc95dbffdfd4c8e6da12c2428f7b9704a17
Jai Prakash <jaiprak@thoughtworks.com>
2015-01-10 15:45:43 +0530

    package.json changed for to run the file deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,10 @@
 npm install
-mkdir data
-node scripts/initializeDB.js data/quiz.db 
-mkdir tests/data
+
+rm -rf data/quiz.db
+node scripts/initializeDB.js data/quiz.db
+rm -rf tests/data/quiz.db
+rm -rf tests/data/quiz.db.backup
 cp data/quiz.db tests/data/quiz.db
 sqlite3 tests/data/quiz.db <scripts/fill_sample_data.sql 
 cp tests/data/quiz.db tests/data/quiz.db.backup
+npm run test
